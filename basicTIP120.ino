1c5fa1bb0b992071defe07e1e7f657ad585b9054
kchrs <costas.christopoulos@gmail.com>
2016-12-05 00:33:46 +0200

    Update basicTIP120.ino

--- basicTIP120.ino
+++ basicTIP120.ino
@@ -1,9 +1,3 @@
-//Example for controlling TIP120 with Attiny85
-//Go through both articles and check programs and steps
-//1 -> https://www.hackster.io/arjun/programming-attiny85-with-arduino-uno-afb829 -> Program attiny85 with arduino 
-//2 -> http://www.instructables.com/id/Use-Arduino-with-TIP120-transistor-to-control-moto/ Use arduino with TIP120 transistor 
-//Combine both, make the connections (I used a 12V - LED tape for running the example) and the result is  a simplified version of the TIP120 example
-
 //The idea is to control high voltage with TIP120 transistor with attiny85 (arduino compatible inexpensive  2 dolar chip :-)
 
 // Define which pin to be used to communicate with Base pin of TIP120 transistor
@@ -13,8 +7,8 @@ void setup() {
 }
 
 void loop() {
-// By changing values from 0 to 255 you can control motor speed or perhaps LED light (eg dimming)
-//Using PWM on ATtiny85 PIN 0. (Code tested with led tape)
+// By changing values from 0 to 255 you can control motor speed or perhaps LED light (eg make it dim) Using PWM on ATtiny85 PIN 0. 
+
   pinMode(TIP120pin, OUTPUT); 
   analogWrite(TIP120pin, 255); 
   delay(5000);
